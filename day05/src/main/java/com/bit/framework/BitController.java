b16ae616539edd98b2747f51f093baaa1e84a3b5
jaeseung <tls1798@naver.com>
2022-05-30 09:51:44 +0900

    22.05.30 create viewResolver

---
+++ day05/src/main/java/com/bit/framework/BitController.java
@@ -0,0 +1,8 @@
+package com.bit.framework;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface BitController {
+	String execute(HttpServletRequest req, HttpServletResponse res) throws Exception;
+}
