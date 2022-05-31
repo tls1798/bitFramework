cdeb10204989f5137c9ab5ca348fdc42fc659b14
jaeseung <tls1798@naver.com>
2022-05-31 11:08:00 +0900

    22.05.31 Spring setting (v3.1.0)

---
+++ sts01/src/test/java/com/bit/frame/WelcomeTest.java
@@ -0,0 +1,24 @@
+package com.bit.frame;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class WelcomeTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
