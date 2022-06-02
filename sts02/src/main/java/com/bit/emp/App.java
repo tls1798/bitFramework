418ca12fe5c5ce8c0a16b4935911c9c25e969394
jaeseung <tls1798@naver.com>
2022-06-02 12:08:15 +0900

    22.06.02 IoC & DI

---
+++ sts02/src/main/java/com/bit/emp/App.java
@@ -0,0 +1,21 @@
+package com.bit.emp;
+
+import java.util.Arrays;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.bit.emp.model.EmpVo;
+
+public class App {
+
+	public static void main(String[] args) {
+		ApplicationContext ac = null;
+		ac = new ClassPathXmlApplicationContext("/applicationContext.xml");
+		EmpVo bean= (EmpVo) ac.getBean("bean");
+		
+		System.out.println(bean);
+
+	}
+
+}
---
+++ sts02/src/test/java/com/bit/emp/StartUpTest.java
@@ -0,0 +1,14 @@
+package com.bit.emp;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StartUpTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
