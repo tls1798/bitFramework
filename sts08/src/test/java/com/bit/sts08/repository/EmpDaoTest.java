2db4d10d7ca3566430343d4b4f78c32aefd2b5af
jaeseung <tls1798@naver.com>
2022-06-13 11:10:34 +0900

    22.06.13 select test

---
+++ sts08/src/test/java/com/bit/sts08/repository/EmpDaoTest.java
@@ -0,0 +1,35 @@
+package com.bit.sts08.repository;
+
+import static org.junit.Assert.*;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:/applicationContext.xml")
+public class EmpDaoTest {
+
+	@Autowired
+	EmpDao empDao;
+	
+	@Test
+	public void test1FindAll() {
+		assertNotNull(empDao.findAll());
+	}
+	
+	@Test
+	public void test2FindOne() {
+		assertNotNull(empDao.findOne(999));
+		log.debug(empDao.findOne(999).toString());
+	}
+	
+}
