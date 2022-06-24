f24921f94a0d0be3a2d06a752f74b8e7b47453bc
jaeseung <tls1798@naver.com>
2022-06-24 17:44:48 +0900

    22.06.24 stomp

---
+++ boot10/src/main/java/com/bit/sts28/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.bit.sts28.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+//	@GetMapping("/")
+//	public String index() {
+//		return "index";
+//	}
+	
+}
---
+++ boot10/src/main/java/com/bit/sts28/domain/Message.java
@@ -0,0 +1,8 @@
+package com.bit.sts28.domain;
+
+import lombok.Data;
+
+@Data
+public class Message {
+	private String msg;
+}
